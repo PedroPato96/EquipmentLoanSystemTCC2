680cef7971d21fbfcb6dc53cfca43a80bf18e954
PedroPato96 <psr1996@hotmail.com>
2024-09-25 19:06:33 -0300

    Alterações e pagina inicial

--- tcc2/src/App.tsx
+++ tcc2/src/App.tsx
@@ -35,6 +35,18 @@ import './theme/variables.css';
 
 setupIonicReact();
 
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
@@ -51,3 +63,4 @@ const App: React.FC = () => (
 );
 
 export default App;
+
--- tcc2/src/pages/Home.tsx
+++ tcc2/src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardContent, IonIcon } from '@ionic/react';
+import { addCircleOutline, listOutline } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,16 +7,26 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Empréstimo de Equipamentos</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardContent>
+            <h2>Bem-vindo ao Sistema de Empréstimo de Equipamentos!</h2>
+            <p>Aqui você pode solicitar e gerenciar o empréstimo de equipamentos da escola.</p>
+          </IonCardContent>
+        </IonCard>
+
+        <IonButton expand="block" color="primary" routerLink="/solicitar-emprestimo">
+          <IonIcon slot="start" icon={addCircleOutline} />
+          Solicitar Empréstimo
+        </IonButton>
+
+        <IonButton expand="block" color="secondary" routerLink="/equipamentos">
+          <IonIcon slot="start" icon={listOutline} />
+          Ver Equipamentos Disponíveis
+        </IonButton>
       </IonContent>
     </IonPage>
   );
